9840341884d36597da49e272fd4662f7a90520e4
Tri Tran <tritran20@gmail.com>
2017-07-08 19:31:05 -0700

    Added completed feature and completed list

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ db = SQLAlchemy(app)
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
-
+        self.completed = False
 
 @app.route('/', methods=['POST', 'GET'])
 def todos():
@@ -25,15 +26,17 @@ def todos():
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Tasks.query.all()
+    tasks = Tasks.query.filter_by(completed=False).all()
+    completed_tasks = Tasks.query.filter_by(completed=True).all()
 
-    return render_template('todos.html', title="Task-List", tasks=tasks)
+    return render_template('todos.html', title="Task-List", tasks=tasks, completed_tasks=completed_tasks)
 
 @app.route('/delete-task', methods=['POST'])
 def remove_task():
     task_id = int(request.form['task-id'])
     task = Tasks.query.get(task_id)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
     return redirect("/")
