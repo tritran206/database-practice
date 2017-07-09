6d9e249d3441dcc6f2aa7882dd1e95f33beb1742
Tri Tran <tritran20@gmail.com>
2017-07-08 17:57:32 -0700

    Incorporated database and add function to TODO list

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://task-list:password@localhost:8889/task-list'
+app.config['SQLALCHEMY_ECHO'] = True
 
-tasks = []
+db = SQLAlchemy(app)
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
 
 @app.route('/', methods=['POST', 'GET'])
 def todos():
 
     if request.method == 'POST':
         task = request.form['task']
-        tasks.append(task)
+        new_task = Tasks(task)
+        db.session.add(new_task)
+        db.session.commit()
+
+    tasks = Tasks.query.all()
 
     return render_template('todos.html', title="Task-List", tasks=tasks)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
